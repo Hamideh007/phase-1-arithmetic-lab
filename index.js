59b6d41b7d9c60e234d80109fc54e9f39704afc1
hamideh rahmani <rahmani.mahtab@gmail.com>
2023-03-16 12:37:23 -0400

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+// Challenge 1: multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // multiply should equal 62
+// Challenge 2: random
+const random = Math.floor(Math.random() * 100) + 1; // generates a random integer between 1 and 100
+// Challenge 3: mod
+const num3 = 24;
+const num4 =5;
+const mod = num3 % num4; // mod should equal 4
+// Challenge 4: max
+const numbers = [5, 10, 20, 15];
+const max = Math.max(...numbers); // max should equal 20
